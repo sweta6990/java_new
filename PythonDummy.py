2c42387e9e42d7eb5829d5476948cb8aa7658326
Sweta <sweta699@gmail.com>
2020-03-11 16:16:59 +0530

    Adding python file

---
+++ PythonDummy.py
@@ -0,0 +1,16 @@
+#! python3
+
+def meth1(n):
+    t =(1,2,3,3,4,2,2,9,8)
+
+    print('Original tuple is ')
+    print(t)
+
+    for i in sorted(t):
+        print('Only sorted below:')
+        print(i)
+	
+
+    for j in sorted(set(t)):
+        print('Sorted and setted..i.e removed duplicates.')
+        print(j)
